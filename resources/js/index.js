396c27827361c0fe8850ec66dbba0ca008dbe996
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-03 17:01:22 +0100

    js carousel room

--- resources/js/index.js
+++ resources/js/index.js
@@ -17,3 +17,50 @@ link.forEach(n => n.addEventListener("click", (e) => {
     navMenu.classList.remove("active");
     search.classList.remove("active");
 }))
+
+
+// Carousel Room
+const cards = document.getElementsByClassName("card");
+const nextButton = document.getElementById("btn-next");
+const prevButton = document.getElementById("btn-prev");
+let position = 0;
+const numberOfSlides = cards.length;
+
+function hideAllSlides() {
+    // remove all slides not currently being viewed
+    for (const card of cards) {
+        card.classList.remove("card-item-visible");
+        card.classList.add("card-item-hidden");
+    }
+}
+
+const handleMoveToNextSlide = function (e) {
+    hideAllSlides();
+
+    // check if last slide has been reached
+    if (position === numberOfSlides - 1) {
+        position = 0; // go back to first slide
+    } else {
+        // move to next slide
+        position++;
+    }
+    // make current slide visible
+    slides[position].classList.add("card-item-visible");
+}
+
+const handleMoveToPrevSlide = function (e) {
+    hideAllSlides();
+
+    // check if we're on the first slide
+    if (position === 0) {
+        position = numberOfSlides - 1; // move to the last slide
+    } else {
+        // move back one
+        position--;
+    }
+    // make current slide visible
+    slides[position].classList.add("card-item-visible");
+}
+
+nextButton.addEventListener("click", handleMoveToNextSlide);
+prevButton.addEventListener("click", handleMoveToPrevSlide);
